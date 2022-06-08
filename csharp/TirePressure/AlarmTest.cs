b2319868998479f16cf8fbbc20e6eef9418346eb
emily <emily@bacheconsulting.com>
2022-06-08 08:24:07 +0200

    csharp version

---
+++ csharp/TirePressure/AlarmTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TirePressure
+{
+    public class AlarmTest
+    {
+        [Fact]
+        public void AlarmIsOffByDefault()
+        {
+            Alarm alarm = new Alarm();
+            Assert.False(alarm.AlarmOn);
+        }
+    }
+}
