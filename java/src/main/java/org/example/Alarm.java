34ed98d9ff93c0097a5bdb0c52458f0651947bd3
emily <emily@bacheconsulting.com>
2023-01-22 14:48:10 +0100

    tweak starting position

--- java/src/main/java/org/example/Alarm.java
+++ java/src/main/java/org/example/Alarm.java
@@ -5,10 +5,9 @@ public class Alarm {
     private static final double LOW_PRESSURE_THRESHOLD = 17;
     private static final double HIGH_PRESSURE_THRESHOLD = 21;
 
-    Sensor sensor = new Sensor();
+    protected Sensor sensor = new Sensor();
 
     boolean alarmOn = false;
-    private long alarmCount = 0;
 
 
     public void check()
@@ -18,7 +17,6 @@ public class Alarm {
         if (psiPressureValue < LOW_PRESSURE_THRESHOLD || HIGH_PRESSURE_THRESHOLD < psiPressureValue)
         {
             alarmOn = true;
-            alarmCount += 1;
         }
     }
 
